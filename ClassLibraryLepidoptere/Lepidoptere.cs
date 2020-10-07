941660109c0f461f044df243dccbdbe0a431568e
sthiry2 <sthiry@arfp.asso.fr>
2020-10-07 10:30:26 +0200

    ST Debut Exo Lepidoptere

---
+++ ClassLibraryLepidoptere/Lepidoptere.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryLepidoptere
+{
+    public class Lepidoptere
+    {
+        private StadeDEvolution monStadeCourant;
+        private DateTime dateDeNaissance;
+
+        public bool SeDeplacer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SeMetamorphoser()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
